4226ea497410a2ab83315ea3d034d2730b62bbde
daniel <contractor@lateralaspect.com.au>
2023-06-23 16:35:46 +0800

    up

--- js/template.js
+++ js/template.js
@@ -9,13 +9,10 @@ function toggle() {
 
   const menu0 = document.querySelectorAll(".menu0")[0];
 
-  vivus();
-
   if (menu0.classList.contains("ani1")) {
 
     menu0.classList.add("ani6");
     click.classList.add("ani7");
-
   } else if (menu0.classList.contains("ani6")) {
 
     menu0.classList.remove("ani6");
@@ -25,6 +22,10 @@ function toggle() {
   click.classList.toggle("ani");
   menu0.classList.toggle("disp");
   menu0.classList.toggle("ani1");
+
+  if (!menu0.classList.contains("disp")) {
+    vivus();
+  }
 }
 
 eventListner(window, "scroll", cache, null);
